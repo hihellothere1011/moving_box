619aeb23fda9990389b9db85b6072fb6a7ee0da0
陳恩懂 <austinhandsome@chenendongdeMacBook-Air.local>
2024-08-17 22:36:16 +0800

    1.0 version

---
+++ script.js
@@ -0,0 +1,18 @@
+const box = document.getElementById("box");
+
+function mouse(e) {
+    const offsetx = e.ClientX - box.getBoundingClientRect().left
+    const offsety = e.ClientY - box.getBoundingClientRect().top
+    function mousemove(e) {
+        box.style.left = `${e.clientX - offsetx}px`;
+        box.style.top = `${e.clientY - offsety}px`;
+    }
+    function mousestop(e) {
+        document.body.removeEventListener("mousemove", mousemove)
+        document.body.removeEventListener("mouseup", mousestop)
+    }
+
+    document.body.addEventListener("mousemove", mousemove)
+    document.body.addEventListener("mouseup", mousestop)
+
+}
